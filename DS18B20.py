8799216f8ffd6fcc80e0d37180c9fbb6a1994dd9
Matt Jones <gitcode@magisa.org>
2020-02-02 20:00:48 -0900

    Basic input example from DS18B20

---
+++ DS18B20.py
@@ -0,0 +1,36 @@
+import glob
+import time
+
+
+def main():
+    ID = '28-01192d67a561'
+
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+
+    while True:
+        print(read_temp(device_file))
+        time.sleep(1)
+
+
+def read_temp_raw(device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp(device_file):
+    lines = read_temp_raw(device_file)
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw(device_file)
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+
+main()
